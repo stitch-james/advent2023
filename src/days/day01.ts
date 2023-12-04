f32981bdc8c33594215a2615db4f39986024ee57
James Allen <james@stitchhealth.co>
2023-12-04 19:02:02 +0000

    Add structure to run a day

---
+++ src/days/day01.ts
@@ -0,0 +1,7 @@
+export function part1() {
+  console.log('Hello from day 1, part 1!');
+}
+
+export function part2() {
+  console.log('Hello from day 1, part 2!');
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import * as day01 from "./days/day01";
+
+const days = [
+  day01,
+];
+
+const day = days[parseInt(process.argv[2]) - 1];
+
+day.part1();
+day.part2();
