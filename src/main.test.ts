bbe0d5cf532b9481a62fb6578bba36eea8282fff
James Allen <james@stitchhealth.co>
2023-12-18 08:08:29 +0000

    Declare type of test inputs

--- src/main.test.ts
+++ src/main.test.ts
@@ -6,12 +6,14 @@ interface TestInput {
   part2: number,
 }
 
-test.each([
+const testValues: TestInput[] = [
   {dayInt: 1, part1: 142, part2: 281},
   {dayInt: 2, part1: 8, part2: 2286},
   {dayInt: 3, part1: 4361, part2: 467835},
   {dayInt: 4, part1: 13, part2: 30},
-])('gets correct answer from example data, day $dayInt', ({ dayInt, part1, part2 }: TestInput) => {
+]
+
+test.each(testValues)('gets correct answer from example data, day $dayInt', ({ dayInt, part1, part2 }: TestInput) => {
   const day = days[dayInt - 1];
   expect(day.part1()).toBe(part1);
   expect(day.part2()).toBe(part2);
